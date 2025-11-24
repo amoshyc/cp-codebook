3f4b24e3fe91666a2d6a10e6454d5e9500126ea2
amoshyc <amoshuangyc@gmail.com>
2025-11-25 00:09:11 +0800

    Disable curly quotes

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-56028d22.js',
+                path_to_root + 'searchindex-f3e0eb48.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
